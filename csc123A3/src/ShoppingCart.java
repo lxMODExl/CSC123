874fdc08098a1dc804bc7eea6489b9f4824dda8e
ricks <ricks@Ricks-PC.lan>
2023-10-17 22:28:43 -0700

    First Commit.

---
+++ csc123A3/src/ShoppingCart.java
@@ -0,0 +1,21 @@
+
+public class ShoppingCart {
+
+	public static void main(String[] args) {
+		String name, vendor;
+		double price, cost, weight, taxRate;
+		
+	}
+	
+	public String book(String book) {
+		return book;
+	}
+	
+	public String food(String food) {
+		return food;
+	}
+	
+	public String general(String general) {
+		return general;
+	}
+}
--- csc123A3/src/csc123A3/ShoppingCart.java
+++
@@ -1,9 +0,0 @@
-package csc123A3;
-
-public class ShoppingCart {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
